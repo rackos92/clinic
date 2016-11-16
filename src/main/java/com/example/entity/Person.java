c4d33edc0de611c38c8e52210e11b90e18089b7f
rackos92 <matejko_92@onet.eu>
2016-11-16 18:59:50 +0100

    16-11-2016

--- src/main/java/com/example/entity/Person.java
+++ src/main/java/com/example/entity/Person.java
@@ -3,13 +3,26 @@ package com.example.entity;
 import javax.persistence.*;
 
 @MappedSuperclass
-public class Person extends Main{
-
-    @Column(name = "name")
+public class Person {
+	
+	private Integer id;
+    //@Column(name = "name")
 	private String name;
-    @Column(name = "lastname")
+    //@Column(name = "lastname")
     private String lastname;
-
+    
+    public Integer getId(){
+    	return id;
+    }
+    
+    public void setId(Integer id){
+    	this.id = id;
+    }
+    
+    public boolean isNew(){
+    	return this.id == null;
+    }
+    
     public String getName(){
     	return name;
     }
